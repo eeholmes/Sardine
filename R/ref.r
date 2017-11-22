c20a99266479e98780f60848deae0b0e4fc911b8
Eli Holmes <eli.holmes@noaa.gov>
2017-11-22 10:13:09 -0800

    Various
    added pre and app to args
    fixed bug in table counting.  Was using str_detect which meant 'tab:' and 'tabA' would be treated the same.

--- R/ref.r
+++ R/ref.r
@@ -3,11 +3,13 @@
 #' Used in the R markdown files for figure and table counters.
 #' 
 #' @param useName name of the label.  Must be of the form type:name, like fig:spawners
+#' @param pre tag to put in front of number.  ref("fig:foo", pre="A") gives A1
+#' @param app tag to put at end of number
 #' 
-#' @return The number of the figure or table
+#' @return The number of the figure or table with pre or app added if needed.
 #'
 #' @details
-#' Can be used anywhere in text or captions.  First time it is called, the counter .refctr is given a number associated with the name and the number returned.  Next time it is called, it checks if the name exists.  If so, it returns its number.  
+#' Can be used anywhere in text or captions.  First time it is called, the counter .refctr is given a number associated with the name and the number returned.  Next time it is called, it checks if the name exists.  If so, it returns its number.  Note, after the first time ref is called, pre and app are ignored.
 #' 
 #' Use like so `r ref("fig:1a")` whereever you need the number.
 #' 
@@ -15,13 +17,14 @@
 #' if(!exists(".rmdenvir")) .rmdenvir = environment()
 #' .rmdenvir and .refctr will be created in the Rmd environment
 #' 
-ref <- function(useName) {
+ref <- function(useName, pre="", app="") {
   require(stringr)
   if(!exists(".refctr")) .refctr <- c(`_` = 0)
   if(any(names(.refctr)==useName)) return(.refctr[useName])
   type=str_split(useName,":")[[1]][1]
-  nObj <- sum(str_detect(names(.refctr),type))
-  useNum <- nObj + 1
+  reftypes=unlist(lapply(str_split(names(.refctr),":"),function(x){x[1]}))
+  nObj <- sum(reftypes==type)
+  useNum <- paste(pre,nObj + 1,app,sep="")
   newrefctr <- c(.refctr, useNum)
   names(newrefctr)[length(.refctr) + 1] <- useName
   assign(".refctr", newrefctr, envir=.rmdenvir)
