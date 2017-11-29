e94428806bb7b5bf6f4dc8225693963377069ecf
Eli Holmes <eli.holmes@noaa.gov>
2017-11-28 19:51:18 -0800

    added man for lagged

--- R/lagged.r
+++ R/lagged.r
@@ -1,5 +1,16 @@
-#How to include lagged variables
-#define a lagged function
+#' Create a k-lagged time series
+#'
+#' Helper function to create a lagged version of a vector.
+#' 
+#' @param x a vector
+#' @param k the lag to use
+#' 
+#' @return A lagged version of the vector
+#' 
+#' @examples
+#' lagged(1:10, 1)
+#' lagged(1:10, -1)
+#' lagged(1:10, 0)
 lagged = function(x, k){
   if(k>=0) return(c(rep(NA,k),x[1:(length(x)-k)]))
   if(k<0) return(c(x[(1-k):length(x)],rep(NA,-k)))
