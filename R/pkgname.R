f2018a564374ebd6818d2f9a1120fbb93dedd09b
Eli Holmes <eli.holmes@noaa.gov>
2017-11-28 19:51:42 -0800

    created man for package

---
+++ R/pkgname.R
@@ -0,0 +1,19 @@
+#' Documentation for Indian oil sardine forecasting papers
+#'
+#' Indian Oil Sardine Data, Code and References for Papers on Forecasting
+#'
+#' To get an overview of the data, see \code{\link{precip}}, \code{\link{oilsardine_qtr}}, and
+#' \code{\link{satellite}}.
+#' To see the code for the data and raw data files, look in the \code{extdata} folder.  The full reference bib file is in \code{Sardine.bib} in the \code{docs}
+#' along with some figures used in the papers.  \code{REFERENCES.bib} is only references for the help files.
+#' To see all the help files type \code{help(package="SardineForecast")}.
+"_PACKAGE"
+#> [1] "_PACKAGE"
+#' @examples
+#' list.files(system.file("docs", package="SardineForecast"))
+#' list.files(system.file("extdata", package="SardineForecast"))
+#' list.files(system.file("extdata/get_satellite_data", package="SardineForecast"))
+#' help(package="SardineForecast")
+#' 
+#' # Show the bibliography
+#' file.show(system.file("docs", "Sardine.bib", package="SardineForecast"))
