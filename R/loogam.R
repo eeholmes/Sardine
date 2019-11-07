c739d186c701e8b03cb7dcc092f999722e348d12
Eli Holmes <eli.holmes@noaa.gov>
2019-11-07 12:24:54 -0800

    add k>1 to loogam

--- R/loogam.R
+++ R/loogam.R
@@ -3,6 +3,7 @@
 #' Helper function to do a LOOCV on a gam model
 #' 
 #' @param mod A model returned by a gam fit
+#' @param k samples. k=1 is LOO, k=2 is LTO
 #' 
 #' @return A list with predictions (fitted), actual, errors (fitted-actual), MAE and RMSE.
 #' 
@@ -11,14 +12,17 @@
 #' dat <- mgcv::gamSim(6,n=100,scale=.5)[,1:2]
 #' m <- mgcv::gam(y~s(x0), data=dat)
 #' loogam(m)$MAE
-loogam <- function(mod){
+loogam <- function(mod, k=1){
   dat <- mod$model
   mod.formula <- mod$formula
-  pred <- NULL
-  for(i in 1:dim(dat)[1]){
-    m <- gam(mod.formula, dat=dat[-1*i,])
-    pred <- c(pred, predict(m, newdata=dat[i,]))
+  pred <- actual <- NULL
+  val <- utils::combn(dim(dat)[1], k)
+  for(j in 1:ncol(val)){
+    i <- val[,j]
+    m <- mgcv::gam(mod.formula, data=dat[-1*i,])
+    pred <- c(pred, mgcv::predict.gam(m, newdata=dat[i,]))
+    actual <- c(actual, dat[i,1])
   }
-  err <- pred-dat[,1]
-  list(pred=pred, actual=dat[,1], err=err, MAE=mean(abs(err)), RMSE=sqrt(mean(err^2)))
+  err <- pred - actual
+  list(pred=pred, actual=actual, err=err, MAE=mean(abs(err)), RMSE=sqrt(mean(err^2)), MdAE=median(abs(err)))
 }
